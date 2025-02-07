8a6c9be7e65fa02877d5c8abcec78b6c329464f0
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-02-07 23:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
   // your code here
+	if(!text){
+		return " ";
+	}
+	let num = text[0];
+	return num;
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+//Uncomment the following line to show e prompt popup
+const text = prompt("Enter text:");
 alert(firstChar(text));
